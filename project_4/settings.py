03b2e13abe55804da2fa0fba59fd72360d50791d
Landiinn <andre.landin17@gmail.com>
2022-10-16 01:32:54 +0000

    adding cloudinary

--- project_4/settings.py
+++ project_4/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'project_4.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATES_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
